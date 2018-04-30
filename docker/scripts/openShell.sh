1c62811e53989a0628e1904e377c585b7d6424eb
Wade Barnes <wade.barnes@shaw.ca>
2018-04-30 08:27:11 -0700

    Update to support specifying a shell.

--- docker/scripts/openShell.sh
+++ docker/scripts/openShell.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 SCRIPT_DIR=$(dirname $0)
 SCRIPTS_DIR="${SCRIPT_DIR}/scripts"
 export MSYS_NO_PATHCONV=1
@@ -9,20 +8,23 @@ export MSYS_NO_PATHCONV=1
 # -----------------------------------------------------------------------------------------------------------------
 usage() {
   cat <<-EOF
-  Opens a bash shell in a given container or image.
+  Opens a shell in a given container or image.
 
-  Usage: ${0} [ -h ] [-c <containername> -i <imagename>] 
+  Usage: ${0} [ -h ] [-c <containername> -i <imagename> -s <shell>] 
 
   OPTIONS:
   ========
     -h prints the usage for the script
 
     -c <containername>
-      Open a bash shell in a running container.
+      Open a shell in a running container.
 
     -i <imagename>
-      Start an image and open a bash shell.
+      Start an image and open a shell.
       The container will be automatically removed on exit.
+
+    -s <shell>
+      The shell to use; bash, sh, etc.  Defaults to bash
 EOF
 exit
 }
@@ -30,10 +32,11 @@ exit
 # -----------------------------------------------------------------------------------------------------------------
 # Initialization:
 # -----------------------------------------------------------------------------------------------------------------
-while getopts c:i:h FLAG; do
+while getopts c:i:s:h FLAG; do
   case $FLAG in
     c ) CONTAINER=$OPTARG ;;
     i ) IMAGE=$OPTARG ;;
+    s ) SHELL_CMD=$OPTARG ;;
     h ) usage ;;
     \?) #unrecognized option - show help
       echo -e \\n"Invalid script option"\\n
@@ -47,14 +50,18 @@ shift $((OPTIND-1))
 if [ -z "${CONTAINER}" ] && [ -z "${IMAGE}" ]; then
   usage
 fi
+
+if [ -z "${SHELL_CMD}" ]; then
+  SHELL_CMD='bash'
+fi
 # =================================================================================================================
 
 if [ ! -z "${CONTAINER}" ]; then
-  echo -e "\nOpening a bash shell to ${CONTAINER} ...\n"
-  winpty docker exec -it ${CONTAINER} bash
+  echo -e "\nOpening a '${SHELL_CMD}' shell to ${CONTAINER} ...\n"
+  winpty docker exec -it ${CONTAINER} ${SHELL_CMD}
 fi
 
 if [ ! -z "${IMAGE}" ]; then
-  echo -e "\nOpening a bash shell to ${IMAGE} ...\n"
-  winpty docker run --rm -it --name ${IMAGE} ${IMAGE} bash
+  echo -e "\nOpening a '${SHELL_CMD}' shell to ${IMAGE} ...\n"
+  winpty docker run --rm -it --name ${IMAGE} ${IMAGE} ${SHELL_CMD}
 fi
