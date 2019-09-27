2bfc5078a3624f947c45e228d9c33358b6fae83b
Wade Barnes <wade.barnes@shaw.ca>
2019-09-27 08:35:26 -0700

    Add support for running connection tests on a pod in OpenShift

--- misc/TestConnection.sh
+++ misc/TestConnection.sh
@@ -7,7 +7,7 @@ OCTOOLSBIN=$(dirname $0)
 # -----------------------------------------------------------------------------------------------------------------
 usage() {
   cat <<-EOF
-  Tests to see if a port is open.
+  Tests to see if a port is open.  The tests can be run locally or remotely on a pod running in OpenShift.
 
   Usage: $0 [ options ] [host:port]
 
@@ -16,10 +16,18 @@ usage() {
 
     $0 -f ./listOfUrisToTest.txt
 
-  OPTIONS:
-  ========
+    $0 -n devex-von-tools -p django google.com:80
+
+    $0 -n devex-von-tools -p django -f ./listOfUrisToTest.txt
+
+  Standard Options:
+
     -f read the connection list from a file
     -h prints the usage for the script
+
+  OpenShift Option:
+    -n The OpenShift namespace containing the pod
+    -p The friendly name of the pod
 EOF
 exit 1
 }
@@ -28,26 +36,44 @@ exit 1
 # =================================================================================================================
 # Funtions:
 # -----------------------------------------------------------------------------------------------------------------
-readList(){
+function readList(){
   (
     if [ -f ${listFile} ]; then
       # Read in the file minus any comments ...
-      echo "Reading list from ${listFile} ..." >&2
+      echo -e \\n"Reading list of hosts and ports from ${listFile} ...\\n" >&2
       _value=$(sed '/^[[:blank:]]*#/d;s/#.*//' ${listFile})
     fi
     echo "${_value}"
   )
 }
+
+# Load openshift-developer-tools functions ...
+_includeFile="ocFunctions.inc"
+if [ ! -z $(type -p ${_includeFile}) ]; then
+  _includeFilePath=$(type -p ${_includeFile})
+  export OCTOOLSBIN=$(dirname ${_includeFilePath})
+
+  if [ -f ${OCTOOLSBIN}/${_includeFile} ]; then
+    . ${OCTOOLSBIN}/${_includeFile}
+  fi
+else
+  _red='\033[0;31m'
+  _yellow='\033[1;33m'
+  _nc='\033[0m' # No Color
+  echo -e \\n"${_red}${_includeFile} could not be found on the path.${_nc}"
+  echo -e "${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}"
+  echo -e "${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"
+fi
 # =================================================================================================================
 
 # =================================================================================================================
 # Initialization:
 # -----------------------------------------------------------------------------------------------------------------
-# In case you wanted to check what variables were passed
-# echo "flags = $*"
-while getopts f:h FLAG; do
+while getopts f:n:p:h FLAG; do
   case $FLAG in
     f) export listFile=$OPTARG ;;
+    n) export FULLY_QUALIFIED_NAMESPACE=$OPTARG ;;
+    p) export podName=$OPTARG ;;
     h ) usage ;;
     \?) #unrecognized option - show help
       echo -e \\n"Invalid script option"\\n
@@ -55,7 +81,6 @@ while getopts f:h FLAG; do
       ;;
   esac
 done
-
 shift $((OPTIND-1))
 
 if [ ! -z "${listFile}" ]; then
@@ -65,8 +90,17 @@ else
 fi
 
 if [ -z "${list}" ]; then
-  echo -e \\n"Missing parameters."\\n
+  echoError \\n"Missing parameters."\\n
   usage
+fi
+
+if [ ! -z "${FULLY_QUALIFIED_NAMESPACE}" ] || [ ! -z "${podName}" ]; then
+  if [ -z "${FULLY_QUALIFIED_NAMESPACE}" ] || [ -z "${podName}" ]; then
+    echoError \\n"When using any of the OpenShift options, you MUST specify them all."\\n
+    usage
+  else
+    runInPod=1
+  fi
 fi
 # =================================================================================================================
 
@@ -79,7 +113,15 @@ for item in ${list}; do
   host=${segments[0]}
   port=${segments[1]}
 
-  echo -n "${host}:${port}"
-  timeout 1 bash -c "cat < /dev/null > /dev/tcp/${host}/${port}" && echo " - Open" || echo " - Closed"
+  _command+="echo -n ${host}:${port} && timeout 1 bash -c \"cat < /dev/null > /dev/tcp/${host}/${port}\" && echo ' - Open' || echo ' - Closed';"
 done
-# =================================================================================================================
+
+if [ -z "${runInPod}" ]; then
+  echo -e "Testing connections from the local machine ..."
+  eval ${_command}
+else
+  echo -e "Testing connections from $(getProjectName)/${podName} ..."
+  runInContainer \
+    "${podName}" \
+    "${_command}"
+fi
