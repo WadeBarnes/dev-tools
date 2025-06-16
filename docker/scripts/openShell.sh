08a385085c142f6ff5b76ba9fe5830a3954f3592
Wade Barnes <wade@neoterictech.ca>
2025-06-16 11:34:16 -0700

    Add user:group option when launching images.
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- docker/scripts/openShell.sh
+++ docker/scripts/openShell.sh
@@ -16,7 +16,7 @@ usage() {
   cat <<-EOF
   Opens a shell in a given container or image.
 
-  Usage: ${0} [ -h ] [-c <containername> -i <imagename> -s <shell>] 
+  Usage: ${0} [ -h ] [-c <containername> -i <imagename> -s <shell>]
 
   OPTIONS:
   ========
@@ -33,6 +33,10 @@ usage() {
       Bind mount a volume.  Only availble with the '-i' option.
       Define in the same way you would with the docker run command.
 
+    -u <user:group>
+      User:Group.  Only availble with the '-i' option.
+      Define in the same way you would with the docker run command.
+
     -s <shell>
       The shell to use; bash, sh, etc.  Defaults to bash
 EOF
@@ -42,12 +46,13 @@ exit
 # -----------------------------------------------------------------------------------------------------------------
 # Initialization:
 # -----------------------------------------------------------------------------------------------------------------
-while getopts c:i:s:v:h FLAG; do
+while getopts c:i:s:v:u:h FLAG; do
   case $FLAG in
     c ) CONTAINER=$OPTARG ;;
     i ) IMAGE=$OPTARG ;;
     s ) SHELL_CMD=$OPTARG ;;
     v ) VOLUME_OPT="-v $OPTARG" ;;
+    u ) USER_OPT="--user $OPTARG" ;;
     h ) usage ;;
     \?) #unrecognized option - show help
       echo -e \\n"Invalid script option"\\n
@@ -75,5 +80,5 @@ fi
 if [ ! -z "${IMAGE}" ]; then
   echo -e "\nOpening a '${SHELL_CMD}' shell to ${IMAGE} ...\n"
   # Override the entrypoint otherwise we could just be trying to issue a command via the container's entrypoint.
-  ${terminalEmu} docker run --rm --net host -it ${VOLUME_OPT} --name $(echo ${IMAGE} | sed 's~\(^.*\):.*$~\1~;s~\(/\|\\\)~\.~g;') --entrypoint ${SHELL_CMD} ${IMAGE} 
+  ${terminalEmu} docker run --rm --net host -it ${USER_OPT} ${VOLUME_OPT} --name $(echo ${IMAGE} | sed 's~\(^.*\):.*$~\1~;s~\(/\|\\\)~\.~g;') --entrypoint ${SHELL_CMD} ${IMAGE}
 fi
